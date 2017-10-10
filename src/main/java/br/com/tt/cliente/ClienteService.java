2d2b35594207d63504663591d2b07d796be4e342
sala03 <sala03@targettrust.local>
2017-10-09 22:40:07 -0300

    Criação da consulta CNPJ

--- src/main/java/br/com/tt/cliente/ClienteService.java
+++ src/main/java/br/com/tt/cliente/ClienteService.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.tt.model.CadastroReceita;
+import br.com.tt.util.HttpClient;
+
 @Service
 public class ClienteService {
 	@Autowired
@@ -29,5 +34,21 @@ public class ClienteService {
 		repository.delete(id);
 		
 	}
+	
+	public CadastroReceita consultaReceita(String cnpj) throws Exception{
+		ObjectMapper mapper = new ObjectMapper();
+		String url = "https://www.receitaws.com.br/v1/cnpj/27865757000102";
+		String receitaJson = HttpClient.get("");
+		CadastroReceita cadReceita = mapper.readValue(receitaJson, CadastroReceita.class);
+		System.out.println(cadReceita.getFantasia());
+		
+		return null;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new ClienteService().consultaReceita("");
+	}
+
 
 }
+
